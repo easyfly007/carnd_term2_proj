8290d46ddb4c87e18fd7aac474af53b7fc35a7d2
easyfly007 <easyfly.huang@gmail.com>
2017-11-09 21:12:03 +0800

    add the ros localization test coe

--- test/ros_localization.py
+++ test/ros_localization.py
@@ -1,10 +1,14 @@
 p = [0, 1, 0, 0, 0]
 world = ['green', 'red', 'green', 'green']
 measurements = ['red', 'green']
+motions = [1,1]
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
-def dense(p, Z):
+def sense(p, Z):
 	q = []
 	for i in range(len(p)):
 		hit = (Z == world[i])
@@ -20,3 +24,8 @@ def move(p, U):
 	U = U % size
 	q = p[-U:] + p[:-U]
 	return q
+
+for meas, mot in zip(measurements, motions):
+	p = sense(p, meas)
+	p = move(p, mot)
+print(p)
