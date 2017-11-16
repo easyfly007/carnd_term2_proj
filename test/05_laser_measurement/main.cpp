7f1ec7908855d60ccd6b0f3c5cc766dd99abb2dc
easyfly007 <easyfly.huang@gmail.com>
2017-11-16 21:16:50 +0800

    update the kalman filter main.cpp code

--- test/05_laser_measurement/main.cpp
+++ test/05_laser_measurement/main.cpp
@@ -11,65 +11,65 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using std::vector;
 
+int main()
+{
+	/*
+	set measurements
+	*/
+	std::vector<MeasurementPackage> measurement_pack_list;
 
-int main() {
+	// hard coded input file with the laser and radar measurements
+	string in_file_name_ = "obj_pose-laser-radar-synthetic-input.txt"
+	ifstream in_file(in_file_name_.c_str(), std::ifstream::in);
 
-	/*******************************************************************************
-	 *  Set Measurements															 *
-	 *******************************************************************************/
-	vector<MeasurementPackage> measurement_pack_list;
-
-	// hardcoded input file with laser and radar measurements
-	string in_file_name_ = "obj_pose-laser-radar-synthetic-input.txt";
-	ifstream in_file(in_file_name_.c_str(),std::ifstream::in);
-
-	if (!in_file.is_open()) {
+	if (!in_file.is_open())
+	{
 		cout << "Cannot open input file: " << in_file_name_ << endl;
 	}
 
 	string line;
-	// set i to get only first 3 measurments
+	// set i to get only first 3 measurements
 	int i = 0;
-	while(getline(in_file, line) && (i<=3)){
-
+	while (getline(in_file, line) && (i <= 3))
+	{
 		MeasurementPackage meas_package;
-
 		istringstream iss(line);
 		string sensor_type;
-		iss >> sensor_type;	//reads first element from the current line
+		iss >> sensor_type; // read first element from the current line
 		int64_t timestamp;
-		if(sensor_type.compare("L") == 0){	//laser measurement
-			//read measurements
+		if (sensor_type.compare("L") == 0)
+		{
+			// laser measurement
 			meas_package.sensor_type_ = MeasurementPackage::LASER;
 			meas_package.raw_measurements_ = VectorXd(2);
 			float x;
 			float y;
 			iss >> x;
 			iss >> y;
-			meas_package.raw_measurements_ << x,y;
+			meas_package.raw_measurements_ << x, y;
 			iss >> timestamp;
 			meas_package.timestamp_ = timestamp;
 			measurement_pack_list.push_back(meas_package);
-
-		}else if(sensor_type.compare("R") == 0){
-			//Skip Radar measurements
+		}
+		else if (sensor_type.compare("R") == 0)
+		{
+			// skip radar measurements
 			continue;
 		}
-		i++;
-
+		i ++ ;
 	}
 
-	//Create a Tracking instance
+	// create a tracking instance;
 	Tracking tracking;
 
-	//call the ProcessingMeasurement() function for each measurement
 	size_t N = measurement_pack_list.size();
-	for (size_t k = 0; k < N; ++k) {	//start filtering from the second frame (the speed is unknown in the first frame)
+	for (size_t k = 0; k < N; k ++)
+	{
 		tracking.ProcessMeasurement(measurement_pack_list[k]);
-		
 	}
 
-	if(in_file.is_open()){
+	if (in_file.is_open())
+	{
 		in_file.close();
 	}
 	return 0;
