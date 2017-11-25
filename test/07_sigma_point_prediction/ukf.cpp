d6303eab692775e68db6754e0b0688f1923f0e83
easyfly007 <easyfly.huang@gmail.com>
2017-11-25 15:02:42 +0800

    add the augmented sigma points prediction

---
+++ test/07_sigma_point_prediction/ukf.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include "ukf.h"
+
+UKF::UKF() {
+  //TODO Auto-generated constructor stub
+  Init();
+}
+
+UKF::~UKF() {
+  //TODO Auto-generated destructor stub
+}
+
+void UKF::Init() {
+
+}
+
+
+/*******************************************************************************
+* Programming assignment functions: 
+*******************************************************************************/
+
+void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
+
+  //set state dimension
+  int n_x = 5;
+
+  //set augmented dimension
+  int n_aug = 7;
+
+  //create example sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+     Xsig_aug <<
+    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
+      1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
+    2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
+    0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
+    0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
+         0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
+         0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
+
+  //create matrix with predicted sigma points as columns
+  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+
+  double delta_t = 0.1; //time diff in sec
+  double dt2 = delta_t * delta_t;
+/*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+  for (int i = 0; i < 2 * n_aug +1 ; i ++)
+  {
+    double px = Xsig_aug(0, i);
+    double py = Xsig_aug(1, i);
+    double v = Xsig_aug(2, i);
+    double yaw = Xsig_aug(3, i);
+    double yawd = Xsig_aug(4, i)
+    double mu_a = Xsig_aug(5, 6);
+    double mu_yaw = Xsig_aug(6, 7);
+    Xsig_pred(0, i) = Xsig_aug(0, i) + v * cos(yaw) * delta_t + 1/2.0 * dt2 * cos(yaw) * mu_a;
+    Xsig_pred(1, i) = Xsig_aug(1, i) + v * sin(yaw) * delta_t + 1/2.0 * dt2 * sin(yaw) * mu_a;
+    Xsig_pred(2, i) = Xsig_aug(2, i) + 0 + delta_t* mu_a;
+    Xsig_pred(3, i) = Xsig_aug(3, i) + yawd * delta_t + 1 /2.0 * delta_t * mu_yaw;
+    Xsig_pred(4, i) = Xsig_aug(4, i) + delta_t * mu_yaw;  
+  }
+
+  //predict sigma points
+  //avoid division by zero
+  //write predicted sigma points into right column
+  
+
+/*******************************************************************************
+ * Student part end
+ ******************************************************************************/
+
+  //print result
+  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
+
+  //write result
+  *Xsig_out = Xsig_pred;
+
+}
