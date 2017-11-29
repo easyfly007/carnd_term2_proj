8c12bfa03ed77473dd7b3c500de335919445ca03
easyfly007 <easyfly.huang@gmail.com>
2017-11-29 21:56:03 +0800

    add the rmse function implementation

--- proj_unscented_kalman_filter/src/tools.cpp
+++ proj_unscented_kalman_filter/src/tools.cpp
@@ -4,6 +4,7 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using namespace std;
 
 Tools::Tools() {}
 
@@ -15,4 +16,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	size_t dataCnt = estimations.size();
+	assert(estimations.size() == ground_truth.size());
+	assert(estimations.size() > 0);
+	size_t dataDim = estimations(0).size();
+	assert(estimations(0).size() == ground_truth(0).size());
+
+	VectorXd rmse(dataDim);
+	for (int i = 0; i < dataCnt; i ++)
+	{
+		VectorXd residual = estimations[i] - ground_truth[i];
+		residual = residual.array() * residual.array();
+		rmse += residual;
+	}
+
+	rmse = rmse/dataCnt;
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+	return rmse;
 }
