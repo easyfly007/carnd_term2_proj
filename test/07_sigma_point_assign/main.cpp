717423c680863d923679010e5136df09da768eaa
easyfly007 <easyfly.huang@gmail.com>
2017-12-03 20:01:46 +0800

    add the signam points generate codes

--- test/07_sigma_point_assign/main.cpp
+++ test/07_sigma_point_assign/main.cpp
@@ -8,20 +8,12 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using std::vector;
 
-int main() {
-
-	//Create a UKF instance
+int main()
+{
 	UKF ukf;
 
-/*******************************************************************************
-* Programming assignment calls
-*******************************************************************************/
-
-    MatrixXd Xsig = MatrixXd(5, 11);
-    ukf.GenerateSigmaPoints(&Xsig);
-
-    //print result
-    std::cout << "Xsig = " << std::endl << Xsig << std::endl;
-
+	MatrixXd Xsig = MatrixXd(5, 11);
+	ukf.generateSigmaPoints(&Xsig);
+	std::cout << "Xsig = " << std::endl << std::endl;
 	return 0;
 }
--- test/07_sigma_point_assign/ukf.h
+++ test/07_sigma_point_assign/ukf.h
@@ -1,40 +1,24 @@
-#ifndef UKF_H
-#define UKF_H
-#include "Dense"
+#ifdef __UKF_H__
+#define __UKF_H__
+
 #include <vector>
+#include "Ddense"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-class UKF {
+class UKF
+{
 public:
-
-
-  /**
-	 * Constructor
-	 */
 	UKF();
-
-	/**
-	 * Destructor
-	 */
 	virtual ~UKF();
 
-	/**
-	 * Init Initializes Unscented Kalman filter
-	 */
-	void Init();
+	void init();
 
-  /**
-   * Student assignment functions
-   */
-  void GenerateSigmaPoints(MatrixXd* Xsig_out);
-  void AugmentedSigmaPoints(MatrixXd* Xsig_out);
-  void SigmaPointPrediction(MatrixXd* Xsig_out);
-  void PredictMeanAndCovariance(VectorXd* x_pred, MatrixXd* P_pred);
-  void PredictRadarMeasurement(VectorXd* z_out, MatrixXd* S_out);
-  void UpdateState(VectorXd* x_out, MatrixXd* P_out);
-
+	void generateSigmaPoints(MatrixXd *Xsig_out);
+	void augmentedSigmaPoints(MatrixXd *Xsig_out);
+	void predictSigmaPoints(VectorXd *z_pred, MatrixXd *p_pred);
+	void predictMeanAndCovariance(VectorXd *z_out, MatrixXd *s_out);
+	void updateState(VectorXd *x_out, MatrixXd *P_out);
 };
-
-#endif /* UKF_H */
+#endif
