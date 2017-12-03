c17c7c6091c991636648e2e96038eaf79124c6e8
easyfly007 <easyfly.huang@gmail.com>
2017-12-03 22:14:10 +0800

    add the augment sigma point prediction

--- test/07_sigma_point_prediction/main.cpp
+++ test/07_sigma_point_prediction/main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
-#include "Dense"
 #include <vector>
+#include "Dense"
 #include "ukf.h"
 
 using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-using std::vector;
 
-int main() {
-
-	//Create a UKF instance
+int main()
+{
 	UKF ukf;
-
-/*******************************************************************************
-* Programming assignment calls
-*******************************************************************************/
-
-    MatrixXd Xsig_pred = MatrixXd(15, 5);
-    ukf.SigmaPointPrediction(&Xsig_pred);
-
+	MatrixXd Xsig_pred = MatrixXd(5, 15);
+	ukf.sigmaPointPrediction(&Xsig_pred);
 	return 0;
 }
--- test/07_sigma_point_prediction/ukf.cpp
+++ test/07_sigma_point_prediction/ukf.cpp
@@ -1,93 +1,61 @@
 #include <iostream>
 #include "ukf.h"
 
-UKF::UKF() {
-  //TODO Auto-generated constructor stub
-  Init();
+UKF::UKF()
+{
+  init();
 }
 
-UKF::~UKF() {
-  //TODO Auto-generated destructor stub
-}
+UKF::~UKF()
+{}
 
-void UKF::Init() {
+void UKF::init()
+{}
 
-}
-
-
-/*******************************************************************************
-* Programming assignment functions: 
-*******************************************************************************/
-
-void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
-
-  //set state dimension
+void UKF::sigmaPointPrediction(MatrixXd *Xsig_out)
+{
   int n_x = 5;
-
-  //set augmented dimension
   int n_aug = 7;
 
-  //create example sigma point matrix
   MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
-     Xsig_aug <<
-    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
+  Xsig_aug << 
+  5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
       1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
     2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
     0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
     0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
          0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
          0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
-
-  //create matrix with predicted sigma points as columns
-  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
-
-  double delta_t = 0.1; //time diff in sec
-  double dt2 = delta_t * delta_t;
-/*******************************************************************************
- * Student part begin
- ******************************************************************************/
-    for (int i = 0; i < 15; i ++)
-  {
-      VectorXd onePoint = VectorXd(5);
-      double px   = Xsig_aug(0, i);
-      double py   = Xsig_aug(1, i);
-      double v    = Xsig_aug(2, i);
-      double yaw  = Xsig_aug(3, i);
-      double yawd = Xsig_aug(4, i);
-      double va   = Xsig_aug(5, i);
-      double yawdd= Xsig_aug(6, i);
-      if (abs(yawd) > 1.0e-10)
-      {
-          Xsig_pred(0, i) = px + v / yawd * (sin(yaw + yawd * delta_t) - sin(yaw)) + 1/2 * cos(yaw) * va * delta_t * delta_t;
-          Xsig_pred(1, i) = py + v / yawd * (- cos(yaw + yawd*delta_t) + cos(yaw)) + 1/2 * sin(yaw) * va * delta_t * delta_t;
-          Xsig_pred(2, i) = v + 0 + va * delta_t;
-          Xsig_pred(3, i) = yaw + yawd * delta_t + 1/2 * yawdd * delta_t * delta_t;
-          Xsig_pred(4, i) = yawd + 0 + yawdd * delta_t;
-      }
-      else
-      {
-          Xsig_pred(0, i) = px + v * cos(yaw) * delta_t + 1 /2 * delta_t * delta_t * cos(yaw) * va;
-          Xsig_pred(1, i) = py + v * sin(yaw) * delta_t + 1 /2 * delta_t * delta_t * sin(yaw) * va;
-          Xsig_pred(2, i) = v + 0 + delta_t * va;
-          Xsig_pred(3, i) = yaw + yawd * delta_t + 1/2 * delta_t * delta_t * yawdd;
-          Xsig_pred(4, i) = yawd + 0 + delta_t * yawdd;
-      }
-      
-
-   
-  //predict sigma points
-  //avoid division by zero
-  //write predicted sigma points into right column
   
+  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+  double delta_t = 0.1;
 
-/*******************************************************************************
- * Student part end
- ******************************************************************************/
+  for (int i = 0; i < 2 * n_aug + 1; i ++)
+  {
+    vectorXd onePoint = VectorXd(n_x);
+    double px    = Xsig_aug(0, i);
+    double py    = Xsig_aug(1, i);
+    double v     = Xsig_aug(2, i);
+    double yaw   = Xsig_aug(3, i);
+    double yawd  = Xsig_aug(4, i);
+    double va    = Xsig_aug(5, i);
+    double yawdd = Xsig_aug(6, i);
+    if (fabs(yawd) < 1.0e-5)
+    {
+      Xsig_pred(0, i) = Xsig_aug(0, i) + v * cos(yaw) * delta_t + 0.5 * cos(yaw) * va * delta_t * delta_t;
+      Xsig_pred(1, i) = Xsig_aug(1, i) + v * sin(yaw) * delta_t + 0.5 * sin(yaw) * va * delta_t * delta_t;
+    }
+    else
+    {
+      Xsig_pred(0, i) = Xsig_aug(0, i) + v/yawd *(sin(yaw + delta_t * yawd) - sin(yaw))
+        + 0.5 * cos(yaw) * va * delta_t * delta_t;
+      Xsig_pred(1, i) = Xsig_aug(1, i) + v / yawd * (-cos(yaw+ delta_t * yawd) + cos(yaw))
+        + 0.5 * sin(yaw) * va * delta_t * delta_t;
+    }
 
-  //print result
-  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
-
-  //write result
+    Xsig_pred(2, i) = Xsig_aug(2, i) + va * delta_t;
+    Xsig_pred(3, i) = Xsig_aug(3, i) + yawd * delta_t + 0.5 * yawdd * delta_t * delta_t;
+    Xsig_pred(4, i) = Xsig_aug(4, i) + yawdd * delta_t;
+  }
   *Xsig_out = Xsig_pred;
-
 }
