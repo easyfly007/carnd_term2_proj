2a97133a45845aa060408e88ee0c171470d79048
easyfly007 <easyfly.huang@gmail.com>
2017-12-04 23:15:54 +0800

    add the state update code and class definition

--- test/07_ukf_update/main.cpp
+++ test/07_ukf_update/main.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
-#include "Dense"
 #include <vector>
+#include "Dense"
 #include "ukf.h"
 
 using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-using std::vector;
 
-int main() {
-
-	//Create a UKF instance
+int main()
+{
 	UKF ukf;
-
-/*******************************************************************************
-* Programming assignment calls
-*******************************************************************************/
-    
-    VectorXd x_out = VectorXd(5);
-    MatrixXd P_out = MatrixXd(5, 5);
-    ukf.UpdateState(&x_out, &P_out);
-
+	VectorXd x_out = VectorXd(5);
+	MatrixXd P_out = MatrixXd(5, 5);
+	ukf.updateState(&x_out, &P_out);
 	return 0;
 }
