85251ce42ef639d0f9309bec950ca2d9b9cc51d3
easyfly007 <easyfly.huang@gmail.com>
2017-12-24 12:43:51 +0800

    add the particle filter code

--- test/13_09_move_robot/main.py
+++ test/13_09_move_robot/main.py
@@ -114,5 +114,25 @@ def eval(r, p):
 
 ####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 
+# Make a robot called myrobot that starts at
+# coordinates 30, 50 heading north (pi/2).
+# Have your robot turn clockwise by pi/2, move
+# 15 m, and sense. Then have it turn clockwise
+# by pi/2 again, move 10 m, and sense again.
+#
+# Your program should print out the result of
+# your two sense measurements.
+#
+# Don't modify the code below. Please enter
+# your code at the bottom.
+
 myrobot = robot()
+myrobot.set(30, 50, pi / 2)
+
+myrobot.move(-pi/2, 15.0)
+print(myrobot.sense())
+
+myrobot.move(- pi/2, 10.0)
+print(myrobot.sense())
+
 
