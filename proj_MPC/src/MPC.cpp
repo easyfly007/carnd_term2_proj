fac2a95d9285b2342b9782f5ace0383b0033b2df
easyfly007 <easyfly.huang@gmail.com>
2018-02-10 23:40:06 +0800

    fix prediction bug

--- proj_MPC/src/MPC.cpp
+++ proj_MPC/src/MPC.cpp
@@ -108,7 +108,7 @@ class FG_eval {
       fg[psi_start + i + 1]  = psi1 - (psi0 + delta0 * dt * v0 / Lf);
       fg[v_start + i + 1]    = v1 - (v0 + a0 * dt);
       // fg[cte_start + i + 1]  = cte1 - (f0 - y0 + CppAD::sin(psi0) * dt);
-      fg[cte_start + i + 1]  = cte1 - (y0 - f0 + CppAD::sin(psi0) * dt);
+      fg[cte_start + i + 1]  = cte1 - (y0 - f0 + v0 * CppAD::sin(epsi0) * dt);
       fg[epsi_start + i + 1] = epsi1 - (psi0 - psides0 + v0 * delta0 * dt / Lf);
     }
   }
